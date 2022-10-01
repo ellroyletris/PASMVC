d9e5f657331068c433fb8fed5c6a76e0fc8d41ff
fahkrieal <m.fahkri123@gmail.com>
2022-10-01 10:27:35 +0700

    add fitur simpan

--- src/com/model/model_siswa.java
+++ src/com/model/model_siswa.java
@@ -23,28 +23,28 @@ public class model_siswa implements controller_siswa{
 
     @Override
     public void Simpan(abjr abumat) throws SQLException {
-//             if (abumat.rbLaki.isSelected()) {
-//            jk = "Laki-Laki";
-//        }else {
-//            jk = "Perempuan";
-//        }
-//        try {
-//            Connection con = koneksi.getCon();
-//            String sql = "Insert Into abumat Values(?,?,?,?)";
-//            PreparedStatement prepare = con.prepareStatement(sql);
-//            prepare.setString(1, abumat.txtNIS.getText());
-//            prepare.setString(2, abumat.txtNama.getText());
-//            prepare.setString(3, jk);
-//            prepare.setString(4, (String)abumat.cbJurusan.getSelectedItem());
-//            prepare.executeUpdate();
-//            JOptionPane.showMessageDialog(null, "Data Berhasil disimpan");
-//            prepare.close();
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }finally {
-//            Tampil(abumat);
-//            abumat.setLebarKolom();
-//    }
+             if (abumat.rbLaki.isSelected()) {
+            jk = "Laki-Laki";
+        }else {
+            jk = "Perempuan";
+        }
+        try {
+            Connection con = koneksi.getCon();
+            String sql = "Insert Into abumat Values(?,?,?,?)";
+            PreparedStatement prepare = con.prepareStatement(sql);
+            prepare.setString(1, abumat.txtNIS.getText());
+            prepare.setString(2, abumat.txtNama.getText());
+            prepare.setString(3, jk);
+            prepare.setString(4, (String)abumat.cbJurusan.getSelectedItem());
+            prepare.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Data Berhasil disimpan");
+            prepare.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }finally {
+            Tampil(abumat);
+            abumat.setLebarKolom();
+    }
     }
 
     @Override
@@ -125,25 +125,25 @@ public class model_siswa implements controller_siswa{
 
     @Override
     public void KlikTabel(abjr abumat) throws SQLException {
-        try {
-            int pilih = abumat.tabel.getSelectedRow();
-            if (pilih == -1) {
-                return;
-                
-            }
-            abumat.txtNIS.setText(abumat.tblModel.getValueAt(pilih, 0).toString());
-            abumat.txtNama.setText(abumat.tblModel.getValueAt(pilih, 1).toString());
-            abumat.cbJurusan.setSelectedItem(abumat.tblModel.getValueAt(pilih, 3).toString());
-            jk = String.valueOf(abumat.tblModel.getValueAt(pilih, 2));
-        } catch (Exception e) {
-            
-        }
-        if (abumat.rbLaki.getText().equals(jk)) {
-            abumat.rbLaki.setSelected(true);
-            
-        }else {
-            abumat.rbPerempuan.setSelected(true);
-        }
+//        try {
+//            int pilih = abumat.tabel.getSelectedRow();
+//            if (pilih == -1) {
+//                return;
+//                
+//            }
+//            abumat.txtNIS.setText(abumat.tblModel.getValueAt(pilih, 0).toString());
+//            abumat.txtNama.setText(abumat.tblModel.getValueAt(pilih, 1).toString());
+//            abumat.cbJurusan.setSelectedItem(abumat.tblModel.getValueAt(pilih, 3).toString());
+//            jk = String.valueOf(abumat.tblModel.getValueAt(pilih, 2));
+//        } catch (Exception e) {
+//            
+//        }
+//        if (abumat.rbLaki.getText().equals(jk)) {
+//            abumat.rbLaki.setSelected(true);
+//            
+//        }else {
+//            abumat.rbPerempuan.setSelected(true);
+//        }
     }
 
   
